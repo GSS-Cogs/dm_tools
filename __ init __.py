5a103120cc2d860cda14ed13aee515577eae03a3
LPerryman <leigh.perryman@gsscogs.uk>
2021-03-19 12:41:58 +0000

    adding another method

--- __ init __.py
+++ __ init __.py
@@ -1,4 +1,6 @@
-from dmtools import find_concepts
+
+from dmtools import search_codelists_for_codes
+from dmtools import check_all_codes_in_codelist
 
 
 #python setup.py sdist bdist_wheel
--- dmtools.py
+++ dmtools.py
@@ -2,10 +2,10 @@ import gssutils
 import os
 import pandas as pd
 
-def find_concepts(codes, pth, colnme, dimension):
+def search_codelists_for_codes(codes, pth, colnme, dimension):
     """
     This method searches through a directory of codelist csv files looking for instances of each value within the codes list.
-    It records in what file it finds the instance and outputs a csv file with a list of the codes, the file it cound it in and the number of times it was found.
+    It records in what file it finds the instance and outputs a csv file with a list of the codes, the file it found it in and the number of times it was found.
     It also outputs a csv file giving the percentage of split between the files, which codes were found in. 
     Method assumes directory only has files with extensions .csv and .csv-metadata.json.
     This methods takes as its arguments:
@@ -21,9 +21,6 @@ def find_concepts(codes, pth, colnme, dimension):
         
         entries = os.listdir(pth)     
 
-        out = dimension + "-concept-search"
-        os.mkdir(out)
-
         output = pd.DataFrame(columns=['Code','Filename'])
         for e in entries:
             try:
@@ -36,19 +33,56 @@ def find_concepts(codes, pth, colnme, dimension):
                         del fnd[colnme]
                         fnd['Filename'] = e
                         output = pd.concat([output,fnd])
-            except Exception as e:
-                print(e)
+            except Exception as x:
+                print(x)
+
+        out = dimension + "-code-search"
+        if not os.path.exists(out):
+            os.mkdir(out)
 
         output = output.sort_values(by=['Code'])
         unq = output.groupby('Code').count()
         output = pd.merge(output, unq, on='Code')
         output = output.rename(columns={'Filename_x': 'Filename', 'Filename_y': 'Count'})
-        output.to_csv(f'{out}/{dimension}-concept-search.csv', index=False)
+        output.to_csv(f'{out}/{dimension}-code-search.csv', index=False)
         
         filenamecount = pd.DataFrame(output['Filename'])
         filenamecount = filenamecount.groupby(['Filename']).size().reset_index(name='Counts')
         filenamecount['Percentage'] = ((filenamecount['Counts'] / filenamecount.Counts.sum()) * 100).round(2)
-        filenamecount.to_csv(f'{out}/{dimension}-filename-count.csv', index=False)
+        filenamecount.to_csv(f'{out}/{dimension}-percentage-split.csv', index=False)
     except Exception as e:
         print(e)
 
+
+def check_all_codes_in_codelist(codes, pth, colnme, dimension):
+    """
+
+    """
+    try:
+        print('Search File: ' + pth + '\n')   
+
+        try:
+            df1 = pd.read_csv(pth)
+            df1 = pd.DataFrame(df[colnme])
+            df1 = df1.rename(columns={colnme:'Codelist Codes'})
+            df2 = pd.DataFrame(codes)
+            df2 = df2.rename(columns={0:'Dataset Codes'})
+            if df1['Codelist Codes'].count() >= df2['Dataset Codes'].count():
+                df3 = pd.concat([df1, df2], axis=1)
+                df3['Dataset Codes'] = df3['Dataset Codes'].fillna('NOT FOUND')
+            else:
+                df3 = pd.concat([df2, df1], axis=1, join="inner")
+                df3['Codelist Codes'] = df3['Codelist Codes'].fillna('NOT FOUND')
+    
+            df3 = df3[['Codelist Codes', 'Dataset Codes']]
+
+            out = dimension + "-check_all_codes-search"
+            if not os.path.exists(out):
+                os.mkdir(out)
+        
+            df3.to_csv(f'{out}/{dimension}-code-search.csv', index=False)
+        except Exception as x:
+            print(x)
+
+    except Exception as e:
+        print(e)
